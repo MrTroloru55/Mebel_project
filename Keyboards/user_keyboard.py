0303dda790baafca09ac97b34a2838bdc056189f
VladisVersh <65561998+VladisVersh@users.noreply.github.com>
2025-08-08 23:20:54 +0400

    Add files via upload
    Добавил клавиатуры на работу со списком задач

--- Keyboards/user_keyboard.py
+++ Keyboards/user_keyboard.py
@@ -1,8 +1,26 @@
 from aiogram import types
 
-user_keyboard_1 = [
+user_keyboard_start = [
     [
-        types.InlineKeyboardButton(text='Посмотреть мои задачи', callback_data='tasks_check'),
+        types.InlineKeyboardButton(text='Посмотреть мои задачи', callback_data='tasks_list')
     ]
 ]
-user_keyboard_1 = types.InlineKeyboardMarkup(inline_keyboard=user_keyboard_1)
+user_keyboard_start = types.InlineKeyboardMarkup(inline_keyboard=user_keyboard_start)
+
+user_keyboard_close_task = [
+    [
+        types.InlineKeyboardButton(text='Закрыть задачу', callback_data='task_done')
+    ],
+    [
+        types.InlineKeyboardButton(text='Невозможно выполнить', callback_data='task_failure'),
+    ]
+]
+user_keyboard_close_task = types.InlineKeyboardMarkup(inline_keyboard=user_keyboard_close_task)
+
+def get_confirmation_keyboard(task_id: int):
+    return types.InlineKeyboardMarkup(inline_keyboard=[
+        [
+            types.InlineKeyboardButton(text='Подтвердить', callback_data=f'confirm_{task_id}'),
+            types.InlineKeyboardButton(text='Отмена', callback_data='cancel')
+        ]
+    ])
