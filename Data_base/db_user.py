74529d0e187be028171de631593195458694254a
VladisVersh <65561998+VladisVersh@users.noreply.github.com>
2025-08-08 23:18:10 +0400

    Add files via upload

--- Data_base/db_user.py
+++ Data_base/db_user.py
@@ -37,3 +37,24 @@ async def get_all_tasks(user_id: int):  # Добавляем user_id как ар
         else:
             print(f"Для пользователя {user_id} задачи не найдены.")
         return tasks
+
+async def check_tables_exist():
+    async with aiosqlite.connect(DB_PATH) as connect:
+        cursor = await connect.cursor()
+        await cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        tables = await cursor.fetchall()
+        print("Существующие таблицы:", tables)
+
+async def complete_task(user_id: int, task_id: int):
+    async with aiosqlite.connect(DB_PATH) as connect:
+        cursor = await connect.cursor()
+        # Удаляем связь пользователя с задачей
+        await cursor.execute(
+            "DELETE FROM user_tasks WHERE user_id = ? AND task_id = ?",
+            (user_id, task_id)
+        )
+        # Если задача больше никому не назначена, можно удалить и саму задачу
+        await cursor.execute(
+            "DELETE FROM tasks WHERE task_id NOT IN (SELECT task_id FROM user_tasks)"
+        )
+        await connect.commit()
